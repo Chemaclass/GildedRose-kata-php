38f802636688c42748ad99ae6e7f36f401b23a71
Chemaclass <chemaclass@outlook.es>
2021-02-21 12:00:11 +0100

    Added ConjuredItemQualityUpdater

---
+++ src/ItemQualityUpdater/ConjuredItemQualityUpdater.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose\ItemQualityUpdater;
+
+use GildedRose\Item;
+
+final class ConjuredItemQualityUpdater implements ItemQualityUpdaterInterface
+{
+    public function update(Item $item): Item
+    {
+        $this->verifyCanUpdate($item);
+
+        $sellIn = $item->sellIn();
+        $quality = $item->quality();
+
+        if ($quality > 0) {
+            $quality -= 2;
+        }
+
+        --$sellIn;
+
+        if ($sellIn < 0 && $quality > 0) {
+            $quality -= 2;
+        }
+
+        return Item::conjured($sellIn, $quality);
+    }
+
+    private function verifyCanUpdate(Item $item): void
+    {
+        if (!$this->canUpdate($item)) {
+            throw new \RuntimeException('No es updatable!!! :(');
+        }
+    }
+
+    private function canUpdate(Item $item): bool
+    {
+        return $item->name() === Item::CONJURED_MANA_CAKE;
+    }
+}
